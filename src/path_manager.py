e3b98005faff89793b93d1e2e30aa1d43cc82007
Daniel Franco <daniel.franco.inbox@gmail.com>
2025-06-09 03:03:13 +0100

    Updated path_manager.py

--- src/path_manager.py
+++ src/path_manager.py
@@ -6,6 +6,8 @@ class PathManager:
         self.root_path = abspath(dirname(self.src_path))
         self.data_path = join(self.root_path, 'data')
         self.raw_data_path = join(self.data_path, 'raw')
+        self.clean_data_path = join(self.data_path, 'clean')
+        self.processed_data_path = join(self.data_path, 'processed')
         self.notebook_path = join(self.root_path, 'notebooks')
         self.models_path = join(self.root_path, 'models')
         self.plots_path = join(self.root_path, 'plots')
